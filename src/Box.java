e6aa6d0b24aa7a74245d63fc6381065d03300f5e
Aibek1998 <andypanda9800@gmail.com>
2024-05-25 18:06:07 +0600

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,20 @@
+public class Box <T>{
+    private T name ;
+
+    public Box() {
+        this.name = name;
+    }
+
+    public T getName() {
+        return name;
+    }
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return ""+name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Box <String> box = new Box<>();
+        box.setName("Aibek");
+        Box <Integer> box1 = new Box<>();
+        box1.setName(1998);
+        System.out.println(box+" "+box1);
+
+    }
+}
